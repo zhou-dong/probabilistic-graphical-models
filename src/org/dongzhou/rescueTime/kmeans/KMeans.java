0082d9c62ce5da847040ad93b422c6014c2d4f94
zhou-dong <82224165@qq.com>
2015-11-20 16:09:41 -0600

    update

--- src/org/dongzhou/rescueTime/kmeans/KMeans.java
+++ src/org/dongzhou/rescueTime/kmeans/KMeans.java
@@ -1,17 +1,28 @@
 package org.dongzhou.rescueTime.kmeans;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.dongzhou.rescueTime.Day;
+import org.dongzhou.rescueTime.RescueTime;
 
 public class KMeans {
 
-	public double errors = 0d;
-	public List<Point> points = null;
-	public List<Centroid> centroids = null;
+	public static double errors = 0d;
+	public static List<Point> points = new ArrayList<Point>();
+	public static List<Centroid> centroids = new ArrayList<>();
 
 	public static void main(String args[]) {
+		List<Day> days = RescueTime.getDays();
+		for (Day day : days) {
+			points.add(new Point(day));
+		}
+		for (int i = 0; i < 2; i++) {
+			double d = Math.random();
 
-		
-		
+		}
+		int t = (int) (d * 100);
+		System.out.println(t);
 	}
 
 }
