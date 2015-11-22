bd44bad13cad6d9c337aa6fbcb78a56cb4294f76
zhou-dong <82224165@qq.com>
2015-11-21 23:01:09 -0600

    update

---
+++ src/org/dongzhou/rescueTime/Hmm.java
@@ -0,0 +1,22 @@
+package org.dongzhou.rescueTime;
+
+import com.milowski.hmm.tools.Forward;
+
+//java -jar jhmm.jar model.xml input.txt
+
+/* com.milowski.hmm.tools.Forward - the forward algorithm output
+ * com.milowski.hmm.tools.Generate - generates random output according to your model
+ * com.milowski.hmm.tools.ModelCheck - check the syntax of your model
+ * com.milowski.hmm.tools.Predict - calculate the most likely state for a sequence
+ * com.milowski.hmm.tools.Train - train a model on a set of sequences
+ */
+public class Hmm {
+
+	public static void main(String args[]) {
+
+		Forward.main(new String[2]);
+
+		System.out.println("Hello world!");
+
+	}
+}
